8281b4f128bbf74e8e265805ae37a23900770cfa
Sascha Wagner <wagners@kph.uni-mainz.de>
2017-04-06 13:27:01 +0200

    Parser for the Mensa, outputs Markdown

---
+++ parse_mensa.js
@@ -0,0 +1,44 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+var msg = 'Konnte Mensa-Infos nicht abrufen :-(';
+
+request('http://www.studierendenwerk-mainz.de/speiseplan/frontend/index.php?building_id=1&display_type=1', function (error, response, html) {
+	if (!error && response.statusCode == 200) {
+		var $ = cheerio.load(html, {
+			normalizeWhitespace: true,
+			decodeEntities: true
+		});
+		msg = '# Die Mensa empfiehlt:\n';
+		$('div.vegan_icon').children().each(function(i, element){
+			//console.log($(this));
+			if ($(element).attr('src')) {
+				if (~$(element).attr('src').indexOf('Veggi.png'))
+					$(element).parent().parent().find('span').before(' [Veggi]');
+				if (~$(element).attr('src').indexOf('Vegan.png'))
+					$(element).parent().parent().find('span').before(' [Vegan]');
+			}
+		});
+		$('div.counter_box').each(function(i, element){
+			var a = $(this).text().replace(/\s*\|\s*/g, '\n');
+			// remove parentheses
+			a = a.replace(/\(.+?\)/g, '');
+			// remove empty lines
+			a = a.replace(/^\s*[\r\n]/gm, '');  // no effect currently...
+			// remove multiple spaces
+			a = a.replace(/ +(?= )/g,'');
+			// insert veggie and vegan icon
+			a = a.replace(/\[(Veg.*)\]/g, '![$1](http://www.studierendenwerk-mainz.de/fileadmin/templates/images/speiseplan/$1.png)');
+			// format ausgabe
+			a = a.replace('- Menü', '');
+			a = a.replace(/^\s+(Ausgabe\s\d)\s+(.*)/g, '\n## $1\n$2');
+			//alert($(this));
+			//console.log(a);
+			msg += a + '\n';
+		});
+	} else {
+		console.log(error);
+	}
+
+	console.log(msg);
+});
