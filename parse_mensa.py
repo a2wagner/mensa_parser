fbeea1e1b1f4c558ffd212274239a4588789ab77
Sascha Wagner <wagners@kph.uni-mainz.de>
2017-11-15 12:08:16 +0100

    Refactor Python code, use methods for menu parsing

--- parse_mensa.py
+++ parse_mensa.py
@@ -15,24 +15,39 @@ content = result.content
 soup = BeautifulSoup(content, 'html.parser')
 
 
-pattern = re.compile('Veg..').search
-[v.parent.find('span').insert_before(' [%s]' % m.group(0)) for v in soup.find_all("div", "vegan_icon") for i in v.find_all('img') for m in [pattern(i.get('src'))] if m]
+def get_counters_scrubbed(soup):
+    pattern = re.compile('Veg..').search
+    [v.parent.find('span').insert_before(' [%s]' % m.group(0)) for v in soup.find_all("div", "vegan_icon") for i in v.find_all('img') for m in [pattern(i.get('src'))] if m]
 
-# get all contents of the different counters, remove parentheses
-dishes = [re.sub('\(.+?\)', '', line.strip()) for counter in soup.find_all('div', 'counter_box') for line in counter.stripped_strings]
+    # get all contents of the different counters, remove parentheses
+    dishes = [re.sub('\(.+?\)', '', line.strip()) for counter in soup.find_all('div', 'counter_box') for line in counter.stripped_strings]
 
-# remove empty lines, | spacings, and kJ values etc.
-dishes[:] = [re.sub(r'\|', '\n', line) for line in filter(None, dishes) if not line.lower().startswith(('kj', 'menü'))]
-# remove strange artifacts like multiple spaces, dash for menu counter, and non-breaking spaces
-dishes[:] = [re.sub('\s\s+', ' ', line.rstrip('-').replace(u'\xa0', u' ')) for line in dishes]
-#print(dishes)
-menu = '# Die Mensa empfiehlt:\n'
-menu += ' '.join(dishes)
-menu = re.sub(r'\s*(Ausgabe\s\d)', r'\n \n## \1\n', menu)
-menu = re.sub(r'\n\s', r'\n', menu)
-menu = re.sub(r'\[(Veg.*)\]', r'![\1](http://www.studierendenwerk-mainz.de/fileadmin/templates/images/speiseplan/\1.png)', menu)
-# if Salatbuffet doesn't start in a separate line
-menu = re.sub(r'\)\sSalat', r')\nSalat', menu)
+    # remove empty lines, | spacings, and kJ values etc.
+    dishes[:] = [re.sub(r'\|', '\n', line) for line in filter(None, dishes) if not line.lower().startswith(('kj', 'menü'))]
+    # remove strange artifacts like multiple spaces, dash for menu counter, and non-breaking spaces
+    dishes[:] = [re.sub('\s\s+', ' ', line.rstrip('-').replace(u'\xa0', u' ')) for line in dishes]
+
+    return dishes
+
+def format_day(dishes_list, day_string=''):
+    menu = ''
+    if day_string:
+        menu = '\n \n# %s\n' % day_string
+    else:
+        menu = '# Die Mensa empfiehlt:\n'
+
+    menu += ' '.join(dishes_list)
+    menu = re.sub(r'\s*(Ausgabe\s\d)', r'\n \n## \1\n', menu)
+    menu = re.sub(r'\n\s', r'\n', menu)
+    menu = re.sub(r'\[(Veg.*)\]', r'![\1](http://www.studierendenwerk-mainz.de/fileadmin/templates/images/speiseplan/\1.png)', menu)
+    # if Salatbuffet doesn't start in a separate line
+    menu = re.sub(r'\)\sSalat', r')\nSalat', menu)
+
+    return menu
+
+
+dishes = get_counters_scrubbed(soup)
+menu = format_day(dishes)
 
 print(menu)
 
